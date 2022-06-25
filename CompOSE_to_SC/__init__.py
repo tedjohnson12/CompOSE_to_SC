778ceabb2d279010fd4686220cd488cca5498b2a
Ted Johnson <tedjohnson12@g.ucla.edu>
2022-06-25 08:58:18 -0700

    fix version in __init__.py

--- CompOSE_to_SC/__init__.py
+++ CompOSE_to_SC/__init__.py
@@ -2,6 +2,6 @@ from CompOSE_to_SC.conversion import conv_h5 as convert
 from CompOSE_to_SC.plotting import plot_EOS
 from CompOSE_to_SC.download import download
 
-__version__='1.0.0'
+__version__='1.0.1'
 
 print('See CompOSE (https://compose.obspm.fr/table) and stellarcollapse (https://stellarcollapse.org/equationofstate.html)')
