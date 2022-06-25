f36f45deede63db3a558adb0b7f50f64281c168b
Ted Johnson <tedjohnson12@g.ucla.edu>
2022-06-25 09:02:09 -0700

    increment version number

--- CompOSE_to_SC/__init__.py
+++ CompOSE_to_SC/__init__.py
@@ -2,6 +2,6 @@ from CompOSE_to_SC.conversion import conv_h5 as convert
 from CompOSE_to_SC.plotting import plot_EOS
 from CompOSE_to_SC.download import download
 
-__version__='1.0.1'
+__version__='1.0.2'
 
 print('See CompOSE (https://compose.obspm.fr/table) and stellarcollapse (https://stellarcollapse.org/equationofstate.html)')
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
 name='CompOSE_to_SC',
-version='1.0.1',
+version='1.0.2',
 packages=find_packages()
 )
 
